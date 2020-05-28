3e85a1933763f9247f68de26b20d0b481f0d457a
Valery <vaulin.cpp@gmail.com>
2020-05-28 04:08:02 +0300

    Исправлена работа исключения и его проброс из метода наружу

--- src/main/java/operations/Divide.java
+++ src/main/java/operations/Divide.java
@@ -2,7 +2,6 @@ package operations;
 
 public class Divide {
     private double divideValue;
-    private double save = 0;
 
     public Divide() {
     }
@@ -15,31 +14,14 @@ public class Divide {
         return divideValue;
     }
 
-    public Double getSave() {
-        return save;
-    }
-
     public void setDivideValue(double divideValue) {
         this.divideValue = divideValue;
     }
 
     public double divideValue(double equals) {
-        //Переменная save будет сохранять значение, чтобы можно было продолжить делить, если до этого было деление на 0
-        //чтобы не сбрасывался результат прошлой цепочки вычислений
-        try {
-            save = equals;
-            equals = equals / divideValue;
-
-            if (divideValue == 0) {
-                throw new ArithmeticException("Деление на ноль невозможно ");
-            }
-        } catch (ArithmeticException ex) {
-            System.err.println("Деление на ноль, введите корректное число: " + ex);
-        }
-
         if (divideValue == 0) {
-            return save;
-        } else
-            return equals;
+            throw new ArithmeticException("Деление на ноль невозможно ");
+        }
+        return equals / divideValue;
     }
 }
