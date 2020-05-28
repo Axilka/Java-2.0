96e8c7e9df14fc53da64f8b10b86a336f1058e42
Valery <vaulin.cpp@gmail.com>
2020-05-28 04:13:10 +0300

    Исключены лишние проверки и добавлена проверка исключения деления на ноль

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,44 +5,30 @@ import org.junit.Assert;
 import org.junit.Test;
 import operations.Divide;
 
-
 public class Tests {
-
-
     //Проверка методов деления
-    @Test
-    public void bufferIsNullInDivide() {
-        Divide divide = new Divide();
-        Assert.assertEquals("Буфер должен быть пустым !", 0, divide.getSave(), 0);
-    }
-
-    @Test
-    public void dividerFromMainInsertEquals() {
-        Divide divide = new Divide();
-        Assert.assertNull(divide.getDivideValue());
-    }
-
     @Test
     public void dividerCheckFromMainInsertEquals() {
         Divide divide = new Divide();
         divide.setDivideValue(5);
-        Assert.assertEquals(divide.divideValue(5),1,0);
+        Assert.assertEquals(divide.divideValue(5), 1, 0);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void dividerByNull() {
+        Divide divide = new Divide();
+        divide.setDivideValue(0);
+        divide.divideValue(5);
     }
 
     @Test
     public void dividerCheckNullFromMainInsertEquals() {
         Divide divide = new Divide();
         divide.setDivideValue(0);
-        Assert.assertEquals(divide.divideValue(5),5,0);
+        Assert.assertEquals(divide.divideValue(5), 5, 0);
     }
 
     //Проверка методов вычитания
-    @Test
-    public void minusFromMainInsertEquals() {
-        Minus minus = new Minus();
-        Assert.assertNull(minus.getMinusValue());
-    }
-
     @Test
     public void minusCheckFromMainInsertEquals() {
         Minus minus = new Minus();
@@ -52,29 +38,17 @@ public class Tests {
 
     //Проверка методов умножения
     @Test
-    public void multiplyFromMainInsertEquals() {
-        Multiply multiply = new Multiply();
-        Assert.assertNull(multiply.getMultiplyValue());
-    }
-
-    @Test
     public void multiplyCheckFromMainInsertEquals() {
         Multiply multiply = new Multiply();
         multiply.setMultiplyValue(5);
-        Assert.assertEquals((multiply.multiplyValue(5)),25,0);
+        Assert.assertEquals((multiply.multiplyValue(5)), 25, 0);
     }
 
     //Проверка методов сложения
     @Test
-    public void plusFromMainInsertEquals() {
-        Plus plus = new Plus();
-        Assert.assertNull(plus.getPlusValue());
-    }
-
-    @Test
     public void plusCheckFromMainInsertEquals() {
         Plus plus = new Plus();
         plus.setPlusValue(5);
-        Assert.assertEquals(plus.plusValue(5),10,0);
+        Assert.assertEquals(plus.plusValue(5), 10, 0);
     }
 }
